e8cb15d9f322a10e64a1bad874086315f6de2b9e
Afif Arman <afifarman50@gmail.com>
2024-03-22 21:22:52 +0700

    add  state toggle in fetch post

--- src/lib/api/fetchPost.ts
+++ src/lib/api/fetchPost.ts
@@ -1,20 +1,24 @@
-import { storeState, storeToast } from "$lib/store/state";
+import { state } from '$lib/store/state';
 
 export const fetchPost = async (formData: FormData) => {
-	return await fetch('/api/upload', {
-			method: 'POST',
-			body: formData
-		})
-			.then(async (res) => {
-				if (res.ok) {
-          storeToast().successToast({ text: "Successfully create"})
-          storeState().update(n => n = false)
-          return await res.json();
-        }
-			})
-			.catch((er) => {
-        storeState().update(n => n = false)
-        storeToast().errorToast({ text: "Error: Something went wrong"})
-				console.log(er);
+	state.update((n) => (n = false));
+	const res = await fetch('/api/upload', {
+		method: 'POST',
+		body: formData
+	});
+	if (res.ok) {
+		state.update((n) => (n = false));
+		// successToast({ text: 'Successfully create' });
+		return await res.json();
+	} else {
+		let jsonParse = JSON.parse(await res.text());
+		if (jsonParse.status == 422) {
+			jsonParse.data.name._errors.forEach((v: any) => {
+				console.log(v);
+				// errorToast({ text: 'Error: Something went wrong' });
 			});
-}
+		}
+		state.update((n) => (n = true));
+		throw res;
+	}
+};
