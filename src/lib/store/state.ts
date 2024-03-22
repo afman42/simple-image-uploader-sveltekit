293f196c75614f14029d4beedecf2e26e5e3037a
Afif Arman <afifarman50@gmail.com>
2024-03-22 21:23:44 +0700

    change code state

--- src/lib/store/state.ts
+++ src/lib/store/state.ts
@@ -7,39 +7,36 @@ type IToast = {
 type ToastPayload = { timeout?: number; text: string };
 export type TToastStatus = 'success' | 'warning' | 'error';
 
-
-export const storeState = () => {
-  const state = writable<boolean>(true)
-  return {
-	getToggle: get(state),
-	state,
-}
+export const state = writable<boolean>(true);
 
 export const storeToast = () => {
 	const stateToasts = writable<IToast[]>([]);
 	const defaultTimeout = 2000;
-  const createToast = (text: string, status: TToastStatus): IToast => ({
-    text,
-    status,
-    id: Math.random() * 1000,
-  });
+	const createToast = (text: string, status: TToastStatus): IToast => ({
+		text,
+		status,
+		id: Math.random() * 1000
+	});
 
 	function updateToast(payload: ToastPayload, status: TToastStatus) {
 		const { text, timeout } = payload;
 
 		const toast = createToast(text, status);
 
-    //@ts-ignore
-		stateToasts.update((n) => n.push(toast))
+		//@ts-ignore
+		stateToasts.update((n) => n.push(toast));
 
 		setTimeout(() => {
-			stateToasts.update((n) => n= n.filter((t: IToast) => t.id !== toast.id));
+			stateToasts.update((n) => {
+				n = n.filter((t: IToast) => t.id !== toast.id);
+				return n;
+			});
 		}, timeout ?? defaultTimeout);
 	}
 	return {
-    toasts: stateToasts,
-    lengthToast: get(stateToasts).length,
-    getToast: get(stateToasts),
+		toasts: stateToasts,
+		lengthToast: get(stateToasts).length,
+		getToast: get(stateToasts),
 		successToast(payload: ToastPayload) {
 			updateToast(payload, 'success');
 		},
