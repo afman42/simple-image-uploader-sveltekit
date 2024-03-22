48dc7d64bb0ff70ae177b10a3817bd26d4baeb9d
Afif Arman <afifarman50@gmail.com>
2024-03-22 21:23:18 +0700

    add code type column

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -4,7 +4,8 @@ import { text, sqliteTable } from 'drizzle-orm/sqlite-core';
 import { eq } from 'drizzle-orm';
 
 export const images = sqliteTable('images', {
-	id: text('id').primaryKey().notNull()
+	id: text('id').primaryKey().notNull(),
+	type: text('type').notNull()
 });
 
 export type Image = typeof images.$inferSelect;
@@ -13,10 +14,17 @@ export type NewImage = typeof images.$inferInsert;
 const sqlite = new Database('./src/lib/server/sqlite.db');
 const db = drizzle(sqlite);
 
-export const createImage = async (id: string) => {
-	return await db.insert(images).values({ id }).returning();
+export const createImage = async (id: string, type: string) => {
+	return await db.insert(images).values({ id, type }).returning();
 };
 
 export const readImage = async (id: string) => {
-	return await db.select({ id: images.id }).from(images).where(eq(images.id, id)).limit(1);
+	return await db.select({ id: images.id, type: images.type }).from(images).where(eq(images.id, id)).limit(1);
+	// console.log(
+	// 	await db
+	// 		.select({ id: images.id, type: images.type })
+	// 		.from(images)
+	// 		.where(eq(images.id, id))
+	// 		.limit(1)
+	// );
 };
