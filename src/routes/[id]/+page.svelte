00e97a4988223b9fb09ab1fb6a6c055147f500b8
Afif Arman <afifarman50@gmail.com>
2024-03-23 19:49:49 +0700

    add share link and download file

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import LinkSvg from '$lib/assets/Link.svg';
 	import DownloadSvg from '$lib/assets/download.svg';
-	import type { PageServerData } from '../$types';
-	export let data: PageServerData;
+	import type { PageData } from './$types';
+	export let data: PageData;
+	function ShareLink(e: Event) {
+		e.preventDefault();
+		navigator.clipboard.writeText(window.location + '');
+	}
+	function DownloadFile(e: Event) {
+		e.preventDefault();
+		var link = document.createElement('a');
+		link.setAttribute('download', data?.id + '.' + data?.type);
+		link.href = '/img/' + data?.id + '.' + data?.type;
+		document.body.appendChild(link);
+		link.click();
+		link.remove();
+	}
 </script>
 
 <div
@@ -14,10 +27,12 @@
 
 <div class="flex justify-center items-center space-x-2 mt-2">
 	<button
+		on:click={ShareLink}
 		class="bg-[#3662E3] font-inter drop-shadow-md text-sm font-medium px-4 py-2 rounded-md flex items-center text-white"
 		><img src={LinkSvg} class="mr-2" alt="linkSvg" /> Share</button
 	>
 	<button
+		on:click={DownloadFile}
 		class="bg-[#3662E3] font-inter drop-shadow-md text-sm font-medium px-4 py-2 rounded-md flex items-center text-white"
 		><img src={DownloadSvg} class="mr-2" alt="linkSvg" /> Download</button
 	>
