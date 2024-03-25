b6c4e2ff79e90af437adc0baa7e08419da605579
Afif Arman <afifarman50@gmail.com>
2024-03-25 11:54:18 +0700

    add basepath

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,10 @@ const config = {
 
 	kit: {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: '/basepath/'
+		}
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { searchForWorkspaceRoot } from 'vite';
 import { defineConfig } from 'vitest/config';
 
+const mode = process.env.NODE_ENV;
+
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
@@ -11,5 +13,6 @@ export default defineConfig({
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	base: mode == 'development' ? '/' : '/basepath/'
 });
