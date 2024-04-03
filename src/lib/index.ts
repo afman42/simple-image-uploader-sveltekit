b8df30422bde047ca9eaf7fe6591e7bf6fac5c0e
Afif Arman <afifarman50@gmail.com>
2024-04-03 19:53:48 +0700

    remove component loader

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from './components/Header.svelte';
-export { default as Loader } from './components/Loader.svelte';
 export { default as Toaster } from './components/Toaster.svelte';
 export { fetchPost } from './api/fetchPost';
 export * from "./store/state"
