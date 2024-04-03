4813377ede1ea785e0058370b66091eea810966a
Afif Arman <afifarman50@gmail.com>
2024-04-03 19:54:14 +0700

    old code loader

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import ArrowUpSvg from '$lib/assets/exit.svg';
-	import { Loader, fetchPost, state } from '$lib';
+	import { fetchPost, state } from '$lib';
 	import { goto } from '$app/navigation';
 	let dragCss = false;
 	let inputFile: HTMLInputElement | null = null;
@@ -87,5 +87,19 @@
 		</div>
 	</div>
 {:else}
-	<Loader />
+	<div
+		class="drop-shadow-md bg-white dark:bg-[#212936] h-44 w-2/4 mx-auto flex justify-center items-center rounded-md"
+		style="margin-top: 10%;"
+	>
+		<div class="flex justify-center w-full mx-20 items-center flex-col space-y-4">
+			<span class="font-bold font-inter text-base dark:text-white"
+				>Uploading, <span class="font-medium font-inter text-base dark:text-[#4D5562]"
+					>Please wait..</span
+				></span
+			>
+			<div class="h-2 rounded-lg w-full bg-[#E5E7EB] overflow-hidden">
+				<div class="animate-progress w-full h-full bg-[#3662E3] origin-left-right"></div>
+			</div>
+		</div>
+	</div>
 {/if}
