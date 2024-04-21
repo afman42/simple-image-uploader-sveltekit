f5f71845f48573076ca4e0dc6b6f02726835df26
Afif Arman <afifarman50@gmail.com>
2024-04-21 18:41:20 +0700

    wait is ready component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import Portal from 'svelte-portal';
 	import { Header, Toaster } from '$lib';
 	import '../style.css';
+	import { tick } from 'svelte';
+
+	let completedDocument = false;
+	async function isReady() {
+		await tick();
+		completedDocument = true;
+	}
+	isReady();
 </script>
 
 <svelte:head>
@@ -10,7 +18,11 @@
 
 <main class="min-h-screen bg-[#F9FAFB] dark:bg-[#121826]">
 	<Header />
-	<slot />
+	{#if completedDocument}
+		<slot />
+	{:else}
+		<div class="flex justify-center items-center" style="margin-top:10%;">Loading...</div>
+	{/if}
 </main>
 
 <Portal target="body">
