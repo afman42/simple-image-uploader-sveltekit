a5f5fc7b62d11692d90aeda3d33f809a7d881b8b
Afif Arman <afifarman50@gmail.com>
2024-04-24 14:44:12 +0700

    add try catch and abortsignal

--- src/lib/api/fetchPost.ts
+++ src/lib/api/fetchPost.ts
@@ -1,24 +1,29 @@
 import { state, toasts } from '$lib/store/state';
 
 export const fetchPost = async (formData: FormData) => {
-	state.update((n) => (n = false));
-	toasts.info('Loading... upload', 1000);
-	const res = await fetch('/api/upload', {
-		method: 'POST',
-		body: formData
-	});
-	if (res.ok) {
+	try {
 		state.update((n) => (n = false));
-		toasts.success('Successfully upload', 1000);
-		return await res.json();
-	} else {
-		let jsonParse = JSON.parse(await res.text());
-		if (jsonParse.status == 422) {
-			jsonParse.data.name._errors.forEach((v: string) => {
-				toasts.error(v, 2000);
-			});
+		toasts.info('Loading... upload', 1000);
+		const res = await fetch('/api/upload', {
+			method: 'POST',
+			body: formData,
+			signal: AbortSignal.timeout(2000)
+		});
+		if (res.ok) {
+			state.update((n) => (n = false));
+			toasts.success('Successfully upload', 1000);
+			return await res.json();
+		} else {
+			let jsonParse = JSON.parse(await res.text());
+			if (jsonParse.status == 422) {
+				jsonParse.data.name._errors.forEach((v: string) => {
+					toasts.error(v, 2000);
+				});
+			}
+			state.update((n) => (n = true));
+			throw res;
 		}
-		state.update((n) => (n = true));
-		throw res;
+	} catch (error) {
+		console.error(error);
 	}
 };
