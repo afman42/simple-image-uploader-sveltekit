f399f782de105e752afe27bdcefab9e0b3b97582
Afif Arman <afifarman50@gmail.com>
2024-04-24 14:44:32 +0700

    add error svelte home page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex justify-center items-center min-h-screen flex-col">
+	<h1 class="dark:text-white">{$page.status}: {$page.error.message}</h1>
+	<a href="/" class="dark:text-white underline">Back</a>
+</div>
