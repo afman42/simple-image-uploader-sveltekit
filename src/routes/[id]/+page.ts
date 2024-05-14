688ec42d1cc8cd9fb5fb3b14bc4e96da21bef1c8
Afif Arman <afifarman50@gmail.com>
2024-05-14 20:05:28 +0700

    refactor little bit

--- src/routes/[id]/+page.ts
+++ src/routes/[id]/+page.ts
@@ -3,29 +3,27 @@ import type { PageLoad } from './$types';
 import { toasts } from '$lib';
 
 export const load: PageLoad = async ({ params, fetch }) => {
-	if (params.id) {
-		let type = params.id.endsWith('1')
-			? '.jpeg'
-			: params.id.endsWith('2')
-				? '.jpg'
-				: params.id.endsWith('3')
-					? '.png'
-					: '.gif';
-		toasts.info('Uploading..', 2000);
-		const res = await fetch('/api/readFile?file=' + params.id, {
-			method: 'GET',
-			signal: AbortSignal.timeout(2000)
-		});
-		if (res.ok) {
-			return {
-				id: params.id,
-				type,
-				nameFile: URL.createObjectURL(await res.blob())
-			};
-		} else {
-			toasts.error('Something went wrong', 2000);
-			throw res;
-		}
+	toasts.info('Uploading..', 2000);
+	let type = params.id.endsWith('1')
+		? '.jpeg'
+		: params.id.endsWith('2')
+			? '.jpg'
+			: params.id.endsWith('3')
+				? '.png'
+				: '.gif';
+	const res = await fetch('/api/readFile?file=' + params.id, {
+		method: 'GET',
+		signal: AbortSignal.timeout(2000)
+	});
+	if (res.ok) {
+		return {
+			id: params.id,
+			type,
+			nameFile: URL.createObjectURL(await res.blob())
+		};
+	} else {
+		const response = JSON.parse(await res.text())
+		console.log({ status: res.status, message: response.message});
+		error(404, 'Not found');
 	}
-	error(404, 'Not found');
 };
--- src/routes/api/readFile/+server.ts
+++ src/routes/api/readFile/+server.ts
@@ -2,15 +2,24 @@ import { error, text } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import fs from 'node:fs';
 import { readImage } from '$lib/server';
-import path from "node:path"
+import path from 'node:path';
 
 export const GET: RequestHandler = async (event) => {
 	const file = event.url.searchParams.get('file') ?? '';
-	const name = await readImage(file);
-	if (!name) {
-		throw error(404, 'Not Found');
+	if (file == '') {
+		throw error(404, 'Params not found');
 	}
-  	const pathJoin = path.join(process.cwd(),'src','bucket','img',`${name[0].id}.${name[0].type}`)
+	const name = await readImage(file);
+	if (name[0] === undefined) {
+		throw error(500,'Not found');
+	}
+	const pathJoin = path.join(
+		process.cwd(),
+		'src',
+		'bucket',
+		'img',
+		`${name[0].id}.${name[0].type}`
+	);
 	const data = fs.readFileSync(pathJoin);
 	return new Response(data);
 };
